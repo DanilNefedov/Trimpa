bcf08fb452060f673795c4171680aa8cad92828a
DanilNefedov <ndanil2@ukr.net>
2022-02-10 18:29:38 +0200

    new virsion

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,35 @@
-// import whereIn from './search.js';
-
-const inpuWhere = document.getElementById('from');
-const blockWhere = document.getElementById('where-block');
-const itemWhere = document.querySelectorAll('.where-block__item');	
-
-
-
-const inpuTo = document.getElementById('to');
-const blockTo = document.getElementById('to-block');
-const itemTo = document.querySelectorAll('.to-block__item');
+let cityArr = ['SFO', 'ATL', 'LAX', 'STL', 'PVG', 'MSP', 'NRT', 'JFK'];
 
 
 
 
-inpuWhere.onclick = () => {
-	blockWhere.classList.add("active");
-}
-for (let i = 0 ; i < itemWhere.length; i++) {
-	itemWhere[i].addEventListener('click', function (){
-		document.getElementById('from').value = itemWhere[i].innerHTML;//can use this
-		blockWhere.classList.remove('active');
-		// if(!event()){
-		// 	console.log('w')
-		// }
-	});
-}
+const search = document.getElementById('search');
 
 
 
+search.onclick = function(event){
+    let inp = event.target.closest('input');
+    let arrValue = [];
+    
 
-inpuTo.onclick = () => {
-	blockTo.classList.add("active");
-}
-for (let i = 0 ; i < itemTo.length; i++) {
-	itemTo[i].addEventListener('click', function (){
-		document.getElementById('to').value = itemTo[i].innerHTML;//can use this
-		blockTo.classList.remove('active');
-	});
+    if(!inp) return;
+
+    if(!search.contains(inp)) return;
+
+    inp.value = '';
+
+
+    if(from.childNodes.length > 1){
+       return
+    }else{
+        cityArr.forEach(elem => {
+            let opt = document.createElement('option');
+            opt.value = elem;
+            from.append(opt);
+            console.log(elem);
+        });
+    }
 }
 
 
 
-
-	
-		
-	
-	
